d0ffe70eeafec8312b9d33621adcc3eaa3b51e54
Samuel Ortiz Guevara <1105362447@u.icesi.edu.co>
2024-04-12 17:48:50 -0500

    add Html in Ts v1

--- src/Components/UserInfo/UserInfo.ts
+++ src/Components/UserInfo/UserInfo.ts
@@ -1,3 +1,58 @@
+class card extends HTMLElement {
+	name?: string;
+	image?: string;
+	profileimage?: string;
+
+	constructor() {
+		super(); // always call super() first in the ctor.
+		this.attachShadow({ mode: 'open' });
+	}
+
+	static get observedAttributes() {
+		const attrs: Record<AttributeCard, null> = {
+			name: null,
+			image: null,
+			profileimage: null,
+		};
+		return Object.keys(attrs);
+	}
+	attributeChangedCallback(propName: AttributeCard, oldValue: string | undefined, newValue: string | undefined) {
+		switch (propName) {
+			default:
+				this[propName] = newValue;
+				break;
+		}
+
+		this.render();
+	}
+	connectedCallback() {
+		this.render();
+	}
+	render() {
+
+const htmlString = `
+    <section id="leftSection">
+        <div class="profile">
+            <img src="images/user-icon-black.png" alt="Avatar de Usuario" class="userAvatar">
+            <br>
+            <h5>Usuario</h5>
+            <h2 id="userName">Nombre de Usuario</h2>
+            <button id="editUserNameButton">Editar Nombre de Usuario</button>
+            <div>
+                <h1>Descripción del usuario</h1>
+                <h2 id="profileDescription" contenteditable="false">Tu descripción</h2>
+                <button id="editProfileDescriptionButton">Editar Descripción</button>
+            </div>
+        </div>
+    </section>
+`;
+
+const element = document.getElementById('myElementId');
+if (element) {
+    element.innerHTML = htmlString;
+}
+
+
 // Left //
 
 const userNameElement = document.getElementById("userName");
@@ -34,3 +89,7 @@ editProfileDescriptionButton?.addEventListener("click", () => {
 // Ejemplo de uso
 updateUserName("Nuevo Usuario");
 updateProfileDescription("Descripción del usuario");
+
+}
+
+import {UserInfo} from "UserInfo"; 
